53acd344c6e608118a4f0bcd93f5e28dc490a38f
vsoch <vsochat@stanford.edu>
2015-09-14 15:33:37 -0700

    adding script to compile results

---
+++ ontological_comparison/2.concatenate_results.R
@@ -0,0 +1,17 @@
+# Concatenate scores into one matrix
+
+output_folder = "/scratch/users/vsochat/DATA/BRAINMETA/ontological_comparison/wang_scores"
+input_files = list.files(output_folder,pattern="*.Rda")
+
+# We will put our results in a data frame
+similarities = matrix(nrow=nrow(images),ncol=nrow(images))
+rownames(similarities) = images$image_id
+colnames(similarities) = images$image_id
+
+for (file in input_files){
+  load(file)
+  similarities[result$row,] = result$scores
+}
+
+output_file = cat(output_folder,"/contrast_defined_images_wang.tsv",sep="")
+write.csv(similarities,filename=output_file,sep="\t")
