4e63a8bade2773155624581758474f5675cb238b
vsoch <vsochat@stanford.edu>
2015-09-19 10:39:18 -0700

    adding script to extract all individual scores, all concept nodes

---
+++ ontological_comparison/cluster/3.calculate_reverse_inference_singles.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# This first script will calculate threshold specific probabilities for an entire node,
+# and then one score for all the mean image at the node, and all unique images below it
+# Another script will be used to calculate reverse inference scores for all images at all nodes
+
+from pybraincompare.ontology.inference import save_priors_df, calculate_reverse_inference, calculate_reverse_inference_threshes
+from pybraincompare.mr.datasets import get_standard_mask
+from pybraincompare.compare.mrutils import get_images_df
+from glob import glob
+import pickle
+import pandas
+import re
+import sys
+
+input_image = sys.argv[1]    
+tables_folder = sys.argv[2] # output folder for priors tables
+scores_folder = sys.argv[3] # folder to output individual results to
+priors_folder = sys.argv[4] # folder with all priors
+
+# Load in the image
+standard_mask = get_standard_mask()
+image_df = get_images_df(input_image,standard_mask)
+single_scores = pandas.DataFrame(columns=["ri_ranges","ri_binary","name","count_in","count_out"])
+
+# We get the concepts and group sizes from the original priors pickle folder
+priors_pickles = glob("%s/*.pkl" %priors_folder)
+
+for p in range(0,len(priors_pickles)):
+    print "Calculating RI for priors %s of %s" %(p,len(priors_pickles))
+    group = pickle.load(open(priors_pickles[p],"rb"))
+    nid = group["nid"]
+    # Find the calculated priors tables
+    priors_tables = glob("%s/*%s*" %(tables_folder,nid))
+    priors_tables_ranges_out = [x for x in priors_tables if re.search("out_ranges",x)][0]
+    priors_tables_ranges_in = [x for x in priors_tables if re.search("in_ranges",x)][0]
+    priors_tables_binary = [x for x in priors_tables if x not in priors_tables_ranges_in + priors_tables_ranges_out]
+    priors_tables_binary_out = [x for x in priors_tables_binary if re.search("df_in",x)][0]
+    priors_tables_binary_in = [x for x in priors_tables if re.search("df_out",x)][0]
+    
+    # Read in priors tables
+    priors_in_ranges = pandas.read_pickle(priors_tables_ranges_in)
+    priors_out_ranges = pandas.read_pickle(priors_tables_ranges_out)
+    priors_in_bin = pandas.read_pickle(priors_tables_binary_in)
+    priors_out_bin = pandas.read_pickle(priors_tables_binary_out)
+    in_count = len(group["in"])
+    out_count = len(group["out"])
+    range_table = group["range_table"]
+
+    # Use priors table, calculate a reverse inference score for the image
+    ri_single_ranges = calculate_reverse_inference(image_df,priors_in_ranges,priors_out_ranges,in_count,out_count,range_table)
+    ri_single_binary = calculate_reverse_inference(image_df,priors_in_bin,priors_out_bin,in_count,out_count)
+    single_scores.loc[nid] = [ri_single_ranges,ri_single_binary,group["name"],in_count,out_count]
+
+# Save all to file
+image_id = int(os.path.split(input_image)[-1].replace(".nii.gz",""))
+result = {}
+result["single_scores"] = single_scores
+result["image_id"] = image_id
+result["image_file"] = input_image
+pickle.dump(result,open("%s/%s_riscores.pkl" %(scores_folder,image_id),"wb"))
---
+++ ontological_comparison/cluster/3.run_calculate_reverse_inference_singles.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from glob import glob
+import os
+
+base = "/share/PI/russpold/work/IMAGE_COMPARISON/ONTOLOGICAL_COMPARISON"
+data = "%s/data" %base        # mostly images
+priors_folder = "%s/priors" %(data)
+tables_folder = "%s/priors/tables" %(data)         # folder with priors tables
+scores_folder = "%s/individual_scores" %(data)     # output folder for individual scores
+images_folder = "%s/resampled_z" %data
+
+if not os.path.exists(scores_folder):
+    os.mkdir(scores_folder)
+
+images = glob("%s/*"%images_folder)
+
+# Generate an output file for each image
+for i in range(0,len(images)):
+    input_image = images[i]
+    image_id = os.path.split(images[i])[-1].replace(".nii.gz","")
+    filey = ".jobs/inf_%s.job" %(image_id)
+    filey = open(filey,"w")
+    filey.writelines("#!/bin/bash\n")
+    filey.writelines("#SBATCH --job-name=%s\n" %(image_id))
+    filey.writelines("#SBATCH --output=.out/%s.out\n" %(image_id))
+    filey.writelines("#SBATCH --error=.out/%s.err\n" %(image_id))
+    filey.writelines("#SBATCH --time=2-00:00\n")
+    filey.writelines("#SBATCH --mem=64000\n")
+    filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/3.calculate_reverse_inference_single.py %s %s %s %s" %(input_image, tables_folder, scores_folder, priors_folder))
+    filey.close()
+    os.system("sbatch -p russpold " + ".jobs/inf_%s.job" %(image_id)) 
