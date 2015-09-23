7613089792f644a6b3c86163f8be462533f8a1f5
vsoch <vsochat@stanford.edu>
2015-09-23 11:56:00 -0700

    adding script to run classification-framework

--- ontological_comparison/cluster/classification-framework/2.run_calculate_likelihood.py
+++ ontological_comparison/cluster/classification-framework/2.run_calculate_likelihood.py
@@ -4,14 +4,16 @@ import os
 
 base = "/share/PI/russpold/work/IMAGE_COMPARISON/ONTOLOGICAL_COMPARISON"
 data = "%s/data" %base        # mostly images
-priors_pickles = glob("%s/likelihood/*.pkl" %(data))
+likelihood_pickles = glob("%s/likelihood/*.pkl" %(data))
 tables_folder = "%s/likelihood/tables" %(data) # output folder for likelihood tables
+
+# This first section will make likelihood tables for entire image set. We might use this for interface, but it's not the classification framework
 
 if not os.path.exists(tables_folder):
     os.mkdir(tables_folder)
 
-for p in range(1,len(priors_pickles)):
-    pkl = priors_pickles[p]
+for p in range(1,len(likelihood_pickles)):
+    pkl = likelihood_pickles[p]
     contrast_id = os.path.split(pkl)[-1].split("_")[-1].replace(".pkl","")
     filey = ".jobs/revinf_%s.job" %(contrast_id)
     filey = open(filey,"w")
@@ -21,6 +23,6 @@ for p in range(1,len(priors_pickles)):
     filey.writelines("#SBATCH --error=.out/%s.err\n" %(contrast_id))
     filey.writelines("#SBATCH --time=2-00:00\n")
     filey.writelines("#SBATCH --mem=64000\n")
-    filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/2.calculate_likelihood.py %s %s" %(pkl, tables_folder))
+    filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/classification-framework/2.calculate_likelihood.py %s %s" %(pkl, tables_folder))
     filey.close()
-    os.system("sbatch -p russpold " + ".jobs/revinf_%s.job" %(contrast_id)) 
+    os.system("sbatch -p russpold " + ".jobs/revinf_%s.job" %(contrast_id))
