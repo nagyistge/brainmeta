ce5e33a86377f9a2f318e1e9f4d6087d51ca0f64
vsoch <vsochat@stanford.edu>
2015-09-23 12:04:20 -0700

    adding script for RI

---
+++ ontological_comparison/cluster/classification-framework/4.calculate_reverse_inference.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+# This prepares data for a LOO cross validation procedure:
+# For each image node (defined with the likelihood group pickles above)
+#     For each image: select him to leave out
+#     With the remaining images, calculate likelihood tables, prior, and, RI for the query image
+#     Condition A: With actual labels, do reverse inference procedure for correct/real tags. For each query image:
+# We want to save a pickle/data object with:
+# result["id"]: "trm*"
+# result["prior"]: {"in": 0.98, "out": 0.02}
+# result["query_RI"]: panda data frame with:
+#               reverse_inference_score  bayes_factor
+#  queryimage1
+#  queryimage2
+#  queryimage3
+
+from pybraincompare.ontology.inference import get_likelihood_df, calculate_reverse_inference, calculate_reverse_inference_threshes
+from pybraincompare.mr.datasets import get_standard_mask
+from pybraincompare.compare.mrutils import get_images_df
+import pickle
+import pandas
+import sys
+import os
+
+image = sys.argv[1]             # Full path of input image
+node = sys.argv[2]       # pickle with groups for concept node
+output_pkl = sys.argv[3]        # Path to save output file pickle
+
+group = pickle.load(open(node,"rb"))
+standard_mask = get_standard_mask()
+
+# Remove image from the in and out groups
+in_group = [x for x in group["in"] if x != image]
+out_group = [x for x in group["out"] if x != image]
+
+# We will save to a result object
+result = dict()
+
+# Calculate likelihood tables
+# P(node mental process|activation) = P(activation|mental process) * P(mental process)
+# divided by
+# P(activation|mental process) * P(mental process) + P(A|~mental process) * P(~mental process)
+# P(activation|mental process): my voxelwise prior map
+range_table = group["range_table"]
+likelihood_dfs = get_likelihood_df(group["nid"],in_group,out_group,standard_mask,range_table)
+
+# Calculate reverse inference (threshold specific) scores for the concept node
+# This is the p(activation | mental process)
+like_in_ranges = likelihood_dfs["in_ranges"]
+like_out_ranges = likelihood_dfs["out_ranges"]
+like_in_bin = likelihood_dfs["in_bin"]
+like_out_bin = likelihood_dfs["out_bin"]
+in_count = len(in_group)
+out_count = len(out_group)
+
+# The prior for each group is N/total
+prior_in = in_count / float(in_count+out_count)
+prior_out = out_count / float(in_count+out_count)
+result["prior_in"] = prior_in
+result["prior_out"] = prior_out
+
+# Now calculate the reverse inference scores for the query image
+# This is a reverse inference score, the p(cognitive process | activation in range [x1..x2])
+# p(cognitive process | an activation "level")
+image_df = get_images_df(file_paths=image,mask=standard_mask)
+image_id = os.path.split(image)[1].replace(".nii.gz","")    
+
+# REVERSE INFERENCE TO CLASSIFY IMAGES - USING THRESHOLDS
+# When we provide the range_table: we use the likelihood tables as "lookups" to create a vector of probabilities
+# (one per voxel) matched to the appropriate probability [voxel,threshold] in the priors lookup tables. 
+# We can calculate a score using the "in" likelihood table (the images labeled with the concept)
+# and the "out" likelihood table (everything else). When we generate this score for the query image and compare
+# it to the prior, we can determine if the image adds new evidence for the concept (or not)
+ri_ranges = calculate_reverse_inference(image_df,like_in_ranges,like_out_ranges,in_count,out_count,range_table)
+result["ri_ranges"] = ri_ranges
+result["bayes_factor_ranges_in"] = ri_ranges / prior_in
+result["bayes_factor_ranges_out"] = ri_ranges / prior_out
+
+# REVERSE INFERENCE TO CLASSIFY IMAGES - USING BINARY ACTIVATION THRESHOLD
+# When we don't provide range table, threshold is extracted from priors_* column name
+ri_bin = calculate_reverse_inference(image_df,like_in_bin,like_out_bin,in_count,out_count)
+result["ri_binary"] = ri_bin
+result["bayes_factor_bin_in"] = ri_bin / prior_in
+result["bayes_factor_bin_out"] = ri_bin / prior_out
+
+# Save rest of varibles to result object
+result["in_count"] = in_count
+result["out_count"] = out_count
+result["ranges_table"] = range_table
+result["image"] = image
+result["nid"] = group["nid"]
+result["concept_node"] = node
+result["image_id"] = image_id
+
+# Save result to file
+pickle.dump(result,open(output_pkl,"wb"))
---
+++ ontological_comparison/cluster/classification-framework/4.run_calculate_reverse_inference.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+from glob import glob
+import pickle
+import os
+
+base = "/share/PI/russpold/work/IMAGE_COMPARISON/ONTOLOGICAL_COMPARISON"
+data = "%s/data" %base        # mostly images
+likelihood_pickles = glob("%s/likelihood/*.pkl" %(data))
+scores_folder = "%s/individual_scores" %(data)     # output folder for individual scores
+
+if not os.path.exists(scores_folder):
+    os.mkdir(scores_folder)
+
+# Generate an output file for each image
+for i in range(0,len(likelihood_pickles)):
+    node = likelihood_pickles[i]
+    group = pickle.load(open(node,"rb"))
+    all_images = group["in"] + group["out"]
+    for image in all_images:
+        image_id = os.path.split(image)[1].replace(".nii.gz","")
+        output_pkl = "%s/%s_%s.pkl" %(scores_folder,group["nid"],image_id)
+if 1==1:
+        if not os.path.exists(output_pkl):
+            filey = ".jobs/ri_%s.job" %(image_id)
+            filey = open(filey,"w")
+            filey.writelines("#!/bin/bash\n")
+            filey.writelines("#SBATCH --job-name=%s\n" %(image_id))
+            filey.writelines("#SBATCH --output=.out/%s.out\n" %(image_id))
+            filey.writelines("#SBATCH --error=.out/%s.err\n" %(image_id))
+            filey.writelines("#SBATCH --time=2-00:00\n")
+            filey.writelines("#SBATCH --mem=64000\n")
+            filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/3.calculate_reverse_inference.py %s %s %s" %(image, node, output_pkl))
+            filey.close()
+            os.system("sbatch -p russpold " + ".jobs/ri_%s.job" %(image_id)) 
+
+
+
+# Instead we want to do the following, a LOO cross validation procedure:
+# For each image node (defined with the likelihood group pickles above)
+#     For each image: select him to leave out
+#     With the remaining images, calculate likelihood tables, prior, and, RI for the query image
+#     Condition A: With actual labels, do reverse inference procedure for correct/real tags. For each query image:
+# We want to save a pickle/data object with:
+# result["id"]: "trm*"
+# result["prior"]: {"in": 0.98, "out": 0.02}
+# result["query_RI"]: panda data frame with:
+#               reverse_inference_score  bayes_factor
+#  queryimage1
+#  queryimage2
+#  queryimage3
+
+# Then for analysis:
+# For each image node (defined with the likelihood group pickles above)
+#     For each image: select him to leave out
+#     Condition A: load scores of image for "correct/real" tags
+#     Condition B: randomly select incorrect tags, load scores, compare
+#     For each of the above, calculate a CE score, and compare distributions.
+#     We would want "correct" tags to have higher scores
+# This means that we need, for each node: to save a reverse inference score for ALL query images in the databases (against the node) and to save the priors values (not including the image) and then a reverse inference score.
+
+if not os.path.exists(tables_folder):
+    os.mkdir(tables_folder)
+
+for p in range(1,len(likelihood_pickles)):
+    pkl = likelihood_pickles[p]
+    contrast_id = os.path.split(pkl)[-1].split("_")[-1].replace(".pkl","")
+    filey = ".jobs/revinf_%s.job" %(contrast_id)
+    filey = open(filey,"w")
+    filey.writelines("#!/bin/bash\n")
+    filey.writelines("#SBATCH --job-name=%s\n" %(contrast_id))
+    filey.writelines("#SBATCH --output=.out/%s.out\n" %(contrast_id))
+    filey.writelines("#SBATCH --error=.out/%s.err\n" %(contrast_id))
+    filey.writelines("#SBATCH --time=2-00:00\n")
+    filey.writelines("#SBATCH --mem=64000\n")
+    filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/classification-framework/2.calculate_likelihood.py %s %s" %(pkl, tables_folder))
+    filey.close()
+    os.system("sbatch -p russpold " + ".jobs/revinf_%s.job" %(contrast_id)) 
