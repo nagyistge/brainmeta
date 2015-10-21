065d8ebf318d276151b951e4487c106c13916978
vsoch <vsochat@stanford.edu>
2015-10-20 18:02:17 -0700

    updating likelihood calculation script

--- ontological_comparison/cluster/classification-framework/2.calculate_likelihood.py
+++ ontological_comparison/cluster/classification-framework/2.calculate_likelihood.py
@@ -4,7 +4,7 @@
 # and then one score for all the mean image at the node, and all unique images below it
 # Another script will be used to calculate reverse inference scores for all images at all nodes
 
-from pybraincompare.ontology.inference import save_likelihood_df, calculate_reverse_inference, calculate_reverse_inference_threshes
+from pybraincompare.ontology.inference import get_likelihood_df, calculate_reverse_inference, calculate_reverse_inference_threshes
 from pybraincompare.mr.datasets import get_standard_mask
 from pybraincompare.compare.mrutils import get_images_df
 import pickle
@@ -23,6 +23,23 @@ standard_mask = get_standard_mask()
 # P(activation|mental process) * P(mental process) + P(A|~mental process) * P(~mental process)
 # P(activation|mental process): my voxelwise prior map
 
-# Save likelihood tables to file
+# Save likelihood tables to file, first for the threshold (ranges) method
 range_table = group["range_table"]
-likelihood_df_files = save_likelihood_df(group["nid"],group["in"],group["out"],standard_mask,tables_folder,range_table)
+likelihood_df_files = get_likelihood_df(nid=group["nid"],
+                                        in_images=group["in"],
+                                        out_images=group["out"],
+                                        standard_mask=standard_mask,
+                                        output_folder=tables_folder,
+                                        range_table=range_table,
+                                        method=["ranges"])
+
+# Next, generate likelihood tables for a set of binary thresholds
+for thresh in range(0,14):
+    likelihood_df_files = get_likelihood_df(nid=group["nid"],
+                                        in_images=group["in"],
+                                        out_images=group["out"],
+                                        standard_mask=standard_mask,
+                                        output_folder=tables_folder,
+                                        range_table=range_table,
+                                        method=["binary"],
+                                        threshold=thresh)
