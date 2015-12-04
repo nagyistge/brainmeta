f66984d76a5b6b94b8a32129f870fad88b83977c
vsoch <vsochat@stanford.edu>
2015-12-04 13:01:52 -0800

    finishing up scripts to parse CNP data matrices, need to check on coding of numbers 6 and 9 (likely mean not applicable)

---
+++ wordfish/analysis/cnp/3cnp_sims.py
@@ -0,0 +1,86 @@
+# This script will generate a similarity matrix for all CNP questions based on the actual
+# data, for either all of the subjects/people, or a subset based on having a disorder label
+
+from sklearn.preprocessing import scale
+from scipy.stats import pearsonr
+import pandas
+import pickle
+import sys
+import os
+import re
+
+r = sys.argv[1]
+data_pkl = sys.argv[2]
+disorder_pkl = sys.argv[3]
+question_pkl = sys.argv[4]
+
+sanity_check = False
+
+data = pandas.read_pickle(data_pkl)
+rx = pandas.read_pickle(disorder_pkl)
+questions = pickle.load(open(question_pkl,"rb"))
+behavior_dir = os.path.dirname(data_pkl)
+
+# This means we don't filter down the data
+if r != "all":
+    # Find all subtypes of the disorder
+    exp = re.compile("^%s" %r)
+    disorders = [x for x in rx.columns if exp.match(x)]
+    label =  rx[disorders].sum(axis=1)
+    individuals = label.index[label==1].tolist()
+    # Subset the data to those individuals
+    data = data.loc[individuals,questions]
+else:
+    data = data.loc[:,questions]
+
+# Keep track of how many we have
+N = data.shape[0]
+
+# Function to filter/parse a column
+def parse_column(col):
+    col = col[col.isnull()==False]
+    col = col.astype(float)
+    col = col[col.eq(-9999)==False]
+    return col
+
+# We need at least 2 people
+if N > 1: 
+    # Missing values of "." ... NO.
+    data = data.replace(".",numpy.nan)
+    subset = data.loc[:,~(data.isnull()).all()]
+    if r == "all" and sanity_check == True:
+        filey = open("%s/cnp_unique_values.txt" %(behavior_dir),"w")
+        for c1 in subset.columns:
+            col1 = subset[c1]
+            col1 = parse_column(col1)
+            filey.writelines("Unique values for %s are %s\n" %(c1,col1.unique().tolist()))
+        filey.close()
+    # Create a correlation matrix for subset that doesn't include missing or -9999 values
+    corr = pandas.DataFrame(columns=subset.columns,index=subset.columns)
+    count=1
+    for c1 in subset.columns:
+        print "Parsing %s, %s of %s" %(c1,count,subset.shape[1])
+        col1 = subset[c1]
+        col1 = parse_column(col1)
+        for c2 in subset.columns:
+            if c1!=c2: 
+                col2 = subset[c2]
+                col2 = parse_column(col2)
+                if len(col2)>0 and len(col1)>0:
+                    overlap = col1.index[col1.index.isin(col2.index)]
+                    if len(overlap) > 1:
+                        # Find the overlap
+                        col1_overlap = col1[overlap]
+                        col2_overlap = col2[overlap]
+                        # scale to account for differences in units
+                        corr.loc[c1,c2] = pearsonr(col1_overlap,col2_overlap)[0]
+                    else:
+                        corr.loc[c1,c2] = 0
+                else:
+                    corr.loc[c1,c2] = 0    
+            else:
+                corr.loc[c1,c2] = 1    
+        count+=1
+
+# Save based on group number and N
+corr.to_csv("%s/%s_%s_corr.tsv" %(behavior_dir,r,N),sep="\t")
---
+++ wordfish/analysis/cnp/3run_cnp_sims.py
@@ -0,0 +1,68 @@
+'''
+1run_cnp_sims.py
+
+Your wordfish project home directory should be defined as WORDFISH_HOME
+
+We have already generated vectors that project CNP questions onto different corpus (reddit,
+neurosynth), and then we parsed CNP data and made a matrix of diagnoses. Now we want to 
+generate similarity matrices for the actual CNP question data, for everyone, and for
+different subsets of individuals (with different diagnoses). This script will submit jobs to
+do that.
+
+These analyses have not been integrated into the wordfish pipeline, but will be if
+they are useful.
+
+'''
+
+
+# REPRESENTATIONAL SIMILARITY ANALYSIS ####################################################
+
+from wordfish.utils import mkdir
+import pandas
+import numpy
+import pickle
+import os
+import re
+
+base_dir = os.environ["WORDFISH_HOME"]
+
+# Get analysis output directories
+analysis_dir = mkdir("%s/analysis" %(base_dir))
+behavior_dir = mkdir("%s/behavior" %(analysis_dir))
+scripts_dir = mkdir("%s/scripts" %(base_dir))
+vectors_dir = mkdir("%s/vectors" %(analysis_dir))
+
+# We will filter down to behavioral questions defined in CNP that were mappable to either nsyn or reddit
+vectors_reddit = pandas.read_csv("%s/cnp_vectors_wrt_reddit.tsv" %vectors_dir,sep="\t",index_col=0)
+vectors_nsyn = pandas.read_csv("%s/cnp_vectors_wrt_neurosynth.tsv" %vectors_dir,sep="\t",index_col=0)
+questions = numpy.unique(vectors_reddit.index.tolist() + vectors_nsyn.index.tolist()).tolist()
+question_pkl = "%s/cnp_question_labels.pkl" %behavior_dir
+pickle.dump(questions,open(question_pkl,"wb"))
+
+# Read in the disorder rx data frame
+disorder_pkl = "%s/cnp_rx_matrix.pkl" %behavior_dir
+data_pkl = "%s/CNP_raw.pkl" %behavior_dir
+rx = pandas.read_pickle(disorder_pkl) 
+
+# Function to submit job
+def submit_job(scripts_dir,data_pkl,disorder_pkl,question_pkl,r):
+    filey = "%s/.job/wordfish_%s.job" %(scripts_dir,r)
+    filey = open(filey,"w")
+    filey.writelines("#!/bin/bash\n")
+    filey.writelines("#SBATCH --job-name=wordfish_%s\n" %(r))
+    filey.writelines("#SBATCH --output=.out/%s.out\n" %(r))
+    filey.writelines("#SBATCH --error=.out/%s.err\n" %(r))
+    filey.writelines("#SBATCH --time=2-00:00\n")
+    filey.writelines("#SBATCH --mem=64000\n")
+    filey.writelines("python %s/3cnp_sims.py %s %s %s %s" %(scripts_dir,r,data_pkl,disorder_pkl,question_pkl))
+    filey.close()
+    os.system("sbatch -p russpold " + "%s/.job/wordfish_%s.job" %(scripts_dir,r)) 
+
+# We will first not take subtypes into account, to maximize data, and do based on disorder groups
+exp = re.compile("^[0-9]{3}")
+groups = numpy.unique([y[0] for y in [exp.findall(x) for x in rx.columns if exp.match(x)]]).tolist()
+for g in groups:
+    submit_job(scripts_dir,data_pkl,disorder_pkl,question_pkl,g)
+
+# Run one script to do for all
+submit_job(scripts_dir,data_pkl,disorder_pkl,question_pkl,"all")
