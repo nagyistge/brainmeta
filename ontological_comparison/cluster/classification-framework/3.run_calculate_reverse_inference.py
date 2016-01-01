75d71bc28ac08bf6030daacb7978d85a3dc9bc87
vsoch <vsochat@stanford.edu>
2016-01-01 01:44:27 -0800

    14K jobs on a queue with a max of 1000? :happy: :new: :year:

--- ontological_comparison/cluster/classification-framework/3.run_calculate_reverse_inference.py
+++ ontological_comparison/cluster/classification-framework/3.run_calculate_reverse_inference.py
@@ -15,8 +15,12 @@ if not os.path.exists(scores_folder):
 # Make a ridiculosly long list of likelihood pickle/image id pairs
 pairs = []
 for i in range(0,len(likelihood_pickles)):
+    node = likelihood_pickles[i]
+    group = pickle.load(open(node,"rb"))
+    all_images = group["in"] + group["out"]
     for image in all_images:
         pairs.append([i,image])
+
 pairs_to_run = pairs[:]
 
 while len(pairs_to_run) > 0:
@@ -25,21 +29,20 @@ while len(pairs_to_run) > 0:
         i,image = pairs_to_run.pop(0)
         node = likelihood_pickles[i]
         group = pickle.load(open(node,"rb"))
-        all_images = group["in"] + group["out"]
         image_id = os.path.split(image)[1].replace(".nii.gz","")
         output_pkl = "%s/%s_%s.pkl" %(scores_folder,group["nid"],image_id)
         if not os.path.exists(output_pkl):
-            filey = ".jobs/ri_%s.job" %(image_id)
+            filey = ".jobs/oc_%s_%s.job" %(i,image_id)
             filey = open(filey,"w")
             filey.writelines("#!/bin/bash\n")
             filey.writelines("#SBATCH --job-name=%s\n" %(image_id))
-            filey.writelines("#SBATCH --output=.out/%s.out\n" %(image_id))
-            filey.writelines("#SBATCH --error=.out/%s.err\n" %(image_id))
+            filey.writelines("#SBATCH --output=.out/%s_%s.out\n" %(i,image_id))
+            filey.writelines("#SBATCH --error=.out/%s_%s.err\n" %(i,image_id))
             filey.writelines("#SBATCH --time=2-00:00\n")
             filey.writelines("#SBATCH --mem=64000\n")
             filey.writelines("python /home/vsochat/SCRIPT/python/brainmeta/ontological_comparison/cluster/classification-framework/3.calculate_reverse_inference.py %s %s %s %s" %(image, node, output_pkl, tables_folder))
             filey.close()
-            os.system("sbatch -p russpold " + ".jobs/ri_%s.job" %(image_id))
+            os.system("sbatch -p russpold " + ".jobs/oc_%s_%s.job" %(i,image_id))
 
 
 # This will produce data for a LOO cross validation procedure:
