007eee7c8c0a5ee6a42b000fdac25c49a8b9629e
vsoch <vsochat@stanford.edu>
2016-01-10 18:08:25 -0800

    finished building model for cnp questions

--- wordfish/analysis/cnp/reddit_lda.py
+++ wordfish/analysis/cnp/reddit_lda.py
@@ -1,7 +1,10 @@
 from gensim import corpora, models, similarities
-from wordfish.corpus import get_corpus
+from wordfish.corpus import get_corpus, get_meta
 from wordfish.nlp import sentence2words
 from wordfish.utils import mkdir
+import pickle
+import gensim
+import json
 import os
 base_dir = os.environ["WORDFISH_HOME"]
 dict_dir = mkdir("%s/dict" %(base_dir))
@@ -30,14 +33,35 @@ print(dictionary.token2id)
 # Now we need to generate a bow vector for each document
 def build_corpus(corpus_files,dictionary):
     corpus = []
-    for corpus_file in corpus_files:
+    for c in range(len(corpus_files)):
+        print "Parsing %s of %s" %(c,len(corpus_files))       
+        corpus_file = corpus_files[c]
         text = open(corpus_file,"rb").read().strip("\n")
-        text = " ".join(sentence2words(text))
-        corpus.append(dictionary.doc2bow(text))
+        words = sentence2words(text)
+        corpus.append(dictionary.doc2bow(words))
     return corpus
 
 corpus = build_corpus(corpus_files,dictionary)
 corpora.MmCorpus.serialize('%s/reddit.mm' %models_dir, corpus)
 
 # Latent semantic analysis
-lsi = gensim.models.lsimodel.LsiModel(corpus=corpus, id2word=dictionary, num_topics=400)
+#lsi = gensim.models.lsimodel.LsiModel(corpus=corpus, id2word=dictionary, num_topics=400)
+#lsi.print_topics(10)
+
+# Latent Dirichlet Allocation
+lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=100, update_every=1, chunksize=10000, passes=1)
+lda.print_topics(10)
+
+# Load CNP questions
+cnp_questions = get_meta(base_dir)["cnp"]
+
+# Here is how to transform new text to model
+cnp_lda = dict()
+for cnp_question in cnp_questions:
+    print "Parsing %s" %cnp_question
+    text = json.loads(open(cnp_question,"rb").read().strip("\n"))
+    words = sentence2words(text["text"])
+    doc_bow = dictionary.doc2bow(words)
+    cnp_lda[text["labels"][0]] = lda[doc_bow]
+    
+pickle.dump(cnp_lda,open("%s/cnp_lda.pkl" %models_dir,"wb"))
